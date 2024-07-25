67f9e3505367a04228db9d871caf3dce5bb83124
Sam <samirsundram09@gmail.com>
2024-07-25 05:40:50 +0530

    First commit with rate limit and proxy setup

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const morgan = require('morgan');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const rateLimit = require('express-rate-limit');
+const axios = require('axios');
+
+const app = express();
+
+const PORT = 3005;
+
+const limiter = rateLimit({
+    windowMs: 2 * 60 * 1000,
+    max: 5,
+    // keyGenerator: (req, res) => {
+    //     req.ip,
+    //     console.log("Request ip is",req.ip);
+    // }
+    
+})
+
+app.use(morgan('combined'));
+app.use(limiter);
+// To protect all the routes simple use ```app.use('/', async ()=> {....});
+app.use('/bookingservice', async (req, res, next) => {
+    // console.log(req.headers['x-access-token']);
+    try {
+        const response = await axios.get('http://localhost:3001/api/v1/isauthenticated', {
+            headers: {
+                'x-access-token': req.headers['x-access-token']
+            }
+        })
+        console.log(response.data);
+        if(response.data.success)
+            next();
+        else {
+            return res.status(401).json({
+                message: 'Unauthorized'
+            })
+        }
+    } catch (error) {
+        return res.status(401).json({
+            message: 'Unauthorized'
+        })
+    }
+    
+})
+app.use('/bookingservice', createProxyMiddleware({ target: 'http://localhost:3002/', changeOrigin: true}));
+
+app.get('/home', (req, res) => {
+    return res.json({message: 'OK'});
+})
+
+app.listen(PORT, () => {
+    console.log(`Server started at port ${PORT}`);
+})
